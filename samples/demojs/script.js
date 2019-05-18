240ddeaee887542f721568cd1036bee1bac73d56
imquang <imquang@users.noreply.github.com>
2019-05-18 23:00:17 +0700

    All done!

---
+++ samples/demojs/script.js
@@ -0,0 +1,10 @@
+//Bài 5. Viết 1 function lấy ra 1 số nhỏ nhất trong 1 mảng các số.
+function soNN(x, y, z, f) {
+    return Math.min(x, y, z, f);
+}
+
+//Bài 1. Cho 1 chuỗi số, hãy viết hàm có tác dụng sao chép chuỗi số lên 10 lần, ngăn cách nhau bởi ký tự "-".
+document.write("123")
+for(var i = 1; i<=9; i++) {
+    document.write("-" + "123");
+}
